ee395d45d5187b3aa6e9b84a08111ecba33a8028
Paweł Rubach <pawel.rubach@sorcersoft.com>
2019-11-22 16:00:26 +0100

    Working on tests for alexander

--- knot_polynomials.py
+++ knot_polynomials.py
@@ -1,18 +1,18 @@
 from topoly import *
 
 # importing the file 31.xyz. It is closed knot.
-curve = import_coords('31.xyz')
+curve = import_coords('data/31.xyz')
 
 polynomials = {}
 # calculating the polynomials
-polynomials['Alexander'] = alexander(curve, closure_method='closed', poly_reduce=False)
-polynomials['Conway'] = conway(curve, closure_method='closed', poly_reduce=False)
-polynomials['Jones'] = jones(curve, closure_method='closed', poly_reduce=False)
-polynomials['HOMFLY-PT'] = homfly(curve, closure_method='closed', poly_reduce=False)
-polynomials['Kauffman bracket'] = kauffman_bracket(curve, closure_method='closed', poly_reduce=False)
-polynomials['Kauffman polynomial'] = kauffman_polynomial(curve, closure_method='closed', poly_reduce=False)
-polynomials['BLM/Ho'] = blmho(curve, closure_method='closed', poly_reduce=False)
-polynomials['Yamada'] = yamada(curve, closure_method='closed', poly_reduce=False)
+polynomials['Alexander'] = alexander(curve, closure='closed', poly_reduce=False)
+polynomials['Conway'] = conway(curve, closure='closed', poly_reduce=False)
+polynomials['Jones'] = jones(curve, closure='closed', poly_reduce=False)
+polynomials['HOMFLY-PT'] = homfly(curve, closure='closed', poly_reduce=False)
+polynomials['Kauffman bracket'] = kauffman_bracket(curve, closure='closed', poly_reduce=False)
+polynomials['Kauffman polynomial'] = kauffman_polynomial(curve, closure='closed', poly_reduce=False)
+polynomials['BLM/Ho'] = blmho(curve, closure='closed', poly_reduce=False)
+polynomials['Yamada'] = yamada(curve, closure='closed', poly_reduce=False)
 
 # Printing the polynomials and matching knot from the dictionary. We need the short version of the polynomial
 print("Knot polynomials for the 3_1 knot:")
--- protein_analysis.py
+++ protein_analysis.py
@@ -11,13 +11,13 @@ fp.close()
 curve = import_coords(data)
 
 # analysis of the whole chain using the Alexander polynomial with two-point closure
-print(alexander(curve, closure_method='two_points', translate=True))
+print(alexander(curve, closure='two_points', translate=True))
 
 # analysis of the whole chain using the Alexander polynomial with closed by rays, 100 times
-print(alexander(curve, closure_method='rays', tries=100, translate=True))
+print(alexander(curve, closure='rays', tries=100, translate=True))
 
 # analysis of the subchains and present it as a matrix
-matrix = alexander(curve, matrix=True, closure_method='two_points', translate=True)
+matrix = alexander(curve, matrix=True, closure='two_points', translate=True)
 
 # plotting the matrix and storing it as 'my_matrix.svg'
 plot_matrix(matrix, plot_ofile='my_matrix', plot_format='svg')
