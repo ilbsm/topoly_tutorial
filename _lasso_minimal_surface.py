17b3ad61645ccd1a01c1385e6b28e018f71d7322
Bartosz <bartosz.gren@student.uw.edu.pl>
2020-04-16 01:44:36 +0200

    Ended new functions?

--- _lasso_minimal_surface.py
+++ _lasso_minimal_surface.py
@@ -27,18 +27,23 @@ print('.xyz structure lasso type:', polymer_lasso_type)
 protein_lasso_type = lasso_type(protein_lasso)
 print('.pdb structure lasso type:', protein_lasso_type)
 
-# If you check how „polymer_lasso" ('newdata/lasso.xyz') look like
-# (for example using xyz2pdb) you'll realize that lasso loop is crossed
-# three times, but gained topology type: L1 is lasso with one crossing.
-# It's because of reduction of crossings which are neighbouring and have
-# opposite sign. Crossings are also reduced when they are close to tail 
-# beginning or end. 
+# If you check how „polymer_lasso" ('newdata/lasso.xyz') looks like
+# (i.e. using xyz2pdb function) you'll realize that lasso loop is 
+# crossed three times. But output is L1, which corresponds to lasso 
+# crossed once. It's because of reduction of crossings. Crossing 
+# reduction happens in one of three situations:
+# * Two neighbouring crossings cross the minimal surface from different
+#   sides. Defaultly it happens if crossings are in proximity of less  
+#   than 10 atoms (aminoacids in case of proteins). 
+# * Crossing is close to tail beginning. Defaultly if the distance is
+#   less than 3 atoms (aminoacids).
+# * Crossing is close to tail ending. Defaultly if distance is less
+#   than 3 atoms (aminoacids).
 
 # You can change these parameters using „min_dist". Defaultly 
-# min_dist = [10,3,3], which means that crossings which are closer than 
-# 10 atoms (aminoacids in proteins) or closer than 3 atoms (aminoacids) 
-# to tail beginning or end are reduced. If you do not want any
-# reduction, then use min_dist=[1,1,1]:
+# min_dist = [10,3,3], which refer to distance between: two crossings,
+# crossing and tail beginning, crossing and tail ending.  If you do not 
+# want any reduction, then use min_dist=[1,1,1]:
 
 no_reduction = lasso_type(polymer_lasso, [1,12], min_dist=[1,1,1])
 print('.xyz structure lasso type without reduction:', no_reduction)
@@ -81,23 +86,10 @@ lasso_type(polymer_lasso, [1,12], min_dist=[1,1,1], pic_files=SurfacePlotFormat.
 triangles = make_surface(polymer_lasso, [1,12], precision = PrecisionSurface.LOW,
                          density=DensitySurface.LOW)
 print('Minimal surface mesh coordinates:', triangles)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Output has three levels: it is a list of dictionaries of dictionaries. 
+# It is a list of triangles which build a mesh of minimal surface. Each 
+# triangle is represented by a dictionary. This dictionary has triangle
+# vertices as keys (with names „A", ”B", „C") and values as another dictionary.
+# This last dictionary is a dictionary of space coordinates with keys „x", „y",
+# „z".
 
